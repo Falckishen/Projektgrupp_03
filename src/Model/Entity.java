690adc394c35eb822b7008f3620b0d0302194598
Marsaielda <ida.altenstedt@gmail.com>
2022-09-05 10:30:45 +0200

    new branch entity

--- src/Model/Entity.java
+++ src/Model/Entity.java
@@ -11,14 +11,34 @@ public abstract class Entity {
     private int health;
 
 
-    Entity(int coordX, int coordY, int hitboxWidthRadius, int hitboxHeightRadius, int health){
+    Entity(int coordX, int coordY, int hitboxWidthRadius, int hitboxHeightRadius){
         this.coordX = coordX;
         this.coordY = coordY;
         this.hitboxWidthRadius = hitboxWidthRadius;
         this.hitboxHeightRadius = hitboxHeightRadius;
-        this.health = health;
 
         this.direction = 0;
         this.velocity = 0;
+        this.health = 1;
+    }
+
+    public int getCoordX() {
+        return coordX;
+    }
+
+    public int getCoordY() {
+        return coordY;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setCoordX(int coordX) {
+        this.coordX = coordX;
+    }
+
+    public void setCoordY(int coordY) {
+        this.coordY = coordY;
     }
 }
