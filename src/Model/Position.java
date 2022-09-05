b1ba71f172c4f4c2b52b1255b7e571e5cb22013b
Falckishen <samuelfa@student.chalmers.se>
2022-09-05 11:00:44 +0200

    Skapade setters i Position.java

--- src/Model/Position.java
+++ src/Model/Position.java
@@ -1,6 +1,6 @@
 package Model;
 
-class Position {
+public class Position {
 
     private int x;
     private int y;
@@ -17,5 +17,13 @@ class Position {
 
     public int getY() {
         return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 }
