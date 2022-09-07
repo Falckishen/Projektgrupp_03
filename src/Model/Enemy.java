dbbc733ba271390787448d9a0cece9446a6e12c3
Marsaielda <ida.altenstedt@gmail.com>
2022-09-07 11:48:43 +0200

    test

---
+++ src/Model/Enemy.java
@@ -0,0 +1,5 @@
+package Model;
+
+public interface Enemy {
+    void doAction();
+}
---
+++ src/Model/EntitiesHandler.java
@@ -0,0 +1,47 @@
+package Model;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class EntitiesHandler {
+    private ArrayList<Entity> completeEntityList = new ArrayList<Entity>();
+//    private ArrayList<Player> players = new ArrayList<Player>();
+    private ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+
+ /*   void addPlayer(){
+        Player p = new Player();
+        players.add(p);
+        completeEntityList.add(p);
+    }*/
+
+    void addEnemy(String type){
+        if (type == "Monster"){
+            addMonster();
+        }
+    }
+
+    private void addMonster(){
+ /*       Monster m = new Monster();
+        enemies.add((Enemy) m);
+        completeEntityList.add((Entity) m);
+    */}
+
+    void updateEntities(){
+
+    }
+
+    private void checkCollision(){
+
+    }
+
+    private void updateEnemies(){
+
+    }
+
+    private void updatePlayers(){
+
+    }
+
+
+}
