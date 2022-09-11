811ae20ec386d26844808803b0f6245fff731350
smaug5 <wannelof@student.chalmers.se>
2022-09-11 21:54:21 +0200

    Add monster movement
    Lets a monster find a player and choose a direction to find him/her

---
+++ src/Model/MonsterTest.java
@@ -0,0 +1,25 @@
+package Model;
+
+public class MonsterTest {
+    public static void main(String[] args) {
+        testAngle();
+    }
+
+    public static void testAngle() {
+        // int coordX, int coordY, int hitboxWidthRadius, int hitboxHeightRadius
+        Monster m = new Monster(0,0, 1, 1);
+        System.out.println(m.findDirectionToPosition(new Position(1,1)));
+        System.out.println(m.findDirectionToPosition(new Position(1,0)));
+
+        System.out.println(m.findDirectionToPosition(new Position(1,-1)));
+        System.out.println(m.findDirectionToPosition(new Position(0,-1)));
+        System.out.println(m.findDirectionToPosition(new Position(-1,-1)));
+
+        System.out.println(m.findDirectionToPosition(new Position(-1,0)));
+        System.out.println(m.findDirectionToPosition(new Position(-1, 1)));
+
+        System.out.println(m.findDirectionToPosition(new Position(0,1)));
+
+
+    }
+}
