3019c466bcc44f9f550e2ce4d76addee29ee11d9
smaug5 <wannelof@student.chalmers.se>
2022-09-16 22:50:47 +0200

    Playerweapon, prel monsterattack
    Player now holds weapon. Need to figure out how to implement monsterattacks

--- src/Model/Entities/Player.java
+++ src/Model/Entities/Player.java
@@ -5,15 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import Model.Entities.Entity;
+import Model.Weapons.Weapon;
+import Model.Weapons.WeaponFactory;
 import Utilities.Direction;
 import Utilities.JustAPlaceToKeepThePublicList;
 
 public class Player extends Friendly {
     private final List<Direction> currentPlayerWalkingDirection;
+    private Weapon weapon;
+
 
     Player(int x, int y, List<Direction> currentPlayerWalkingDirection) {
         super(x, y, 25, 25, 5);
         this.currentPlayerWalkingDirection = currentPlayerWalkingDirection;
+        this.weapon = WeaponFactory.getGun(new EntityCreator());
     }
 
     @Override
@@ -31,6 +36,11 @@ public class Player extends Friendly {
                     case RIGHT -> super.setX(super.getCurrentPosition().getX()+super.getSpeed());
                 }
             }
+        }
+    }
+    public void shootAttack() {
+        if(this.weapon != null) {
+            this.weapon.actionShoot();
         }
     }
 }
