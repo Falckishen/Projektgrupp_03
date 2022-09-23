53aa4da924a78edb937292dd4d161e71679b471d
Falckishen <samuelfa@student.chalmers.se>
2022-09-23 16:18:37 +0200

    Ändrade namn i KeyboardInput

--- src/Controller/KeyboardInput.java
+++ src/Controller/KeyboardInput.java
@@ -12,7 +12,7 @@ public class KeyboardInput {
     private boolean rightKeyPressed = false;
     private final ArrayList<Integer> playerInputArrayList;
 
-    public KeyboardInput(JComponent steve, ArrayList<Integer> playerInputArrayList){
+    public KeyboardInput(JComponent jComponent, ArrayList<Integer> playerInputArrayList){
         this.playerInputArrayList = playerInputArrayList;
 
         Action walkUpActionPressed = new WalkUpActionPressed();
@@ -26,27 +26,27 @@ public class KeyboardInput {
         Action spacePressed = new spacePressed();
         Action spaceReleased = new spaceReleased();
 
-        steve.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_W, 0, false),"walkUpActionPressed");
-        steve.getActionMap().put("walkUpActionPressed", walkUpActionPressed);
-        steve.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0, false),"walkLeftActionPressed");
-        steve.getActionMap().put("walkLeftActionPressed", walkLeftActionPressed);
-        steve.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_S, 0, false),"walkDownActionPressed");
-        steve.getActionMap().put("walkDownActionPressed", walkDownActionPressed);
-        steve.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0, false),"walkRightActionPressed");
-        steve.getActionMap().put("walkRightActionPressed", walkRightActionPressed);
-        steve.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_W, 0, true),"walkUpActionReleased");
-        steve.getActionMap().put("walkUpActionReleased", walkUpActionReleased);
-        steve.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0, true),"walkLeftActionReleased");
-        steve.getActionMap().put("walkLeftActionReleased", walkLeftActionReleased);
-        steve.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_S, 0, true),"walkDownActionReleased");
-        steve.getActionMap().put("walkDownActionReleased", walkDownActionReleased);
-        steve.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0, true),"walkRightActionReleased");
-        steve.getActionMap().put("walkRightActionReleased", walkRightActionReleased);
+        jComponent.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_W, 0, false),"walkUpActionPressed");
+        jComponent.getActionMap().put("walkUpActionPressed", walkUpActionPressed);
+        jComponent.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0, false),"walkLeftActionPressed");
+        jComponent.getActionMap().put("walkLeftActionPressed", walkLeftActionPressed);
+        jComponent.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_S, 0, false),"walkDownActionPressed");
+        jComponent.getActionMap().put("walkDownActionPressed", walkDownActionPressed);
+        jComponent.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0, false),"walkRightActionPressed");
+        jComponent.getActionMap().put("walkRightActionPressed", walkRightActionPressed);
+        jComponent.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_W, 0, true),"walkUpActionReleased");
+        jComponent.getActionMap().put("walkUpActionReleased", walkUpActionReleased);
+        jComponent.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0, true),"walkLeftActionReleased");
+        jComponent.getActionMap().put("walkLeftActionReleased", walkLeftActionReleased);
+        jComponent.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_S, 0, true),"walkDownActionReleased");
+        jComponent.getActionMap().put("walkDownActionReleased", walkDownActionReleased);
+        jComponent.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0, true),"walkRightActionReleased");
+        jComponent.getActionMap().put("walkRightActionReleased", walkRightActionReleased);
 
-        steve.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, 0, false),"spacePressed");
-        steve.getActionMap().put("spacePressed", spacePressed);
-        steve.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, 0, true),"spaceReleased");
-        steve.getActionMap().put("spaceReleased", spaceReleased);
+        jComponent.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, 0, false),"spacePressed");
+        jComponent.getActionMap().put("spacePressed", spacePressed);
+        jComponent.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, 0, true),"spaceReleased");
+        jComponent.getActionMap().put("spaceReleased", spaceReleased);
     }
 
     class WalkUpActionPressed extends AbstractAction{
