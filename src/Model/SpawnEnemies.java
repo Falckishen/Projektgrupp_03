2661caa369afe8ae7ea1ef5a153205c29eba067e
Falckishen <samuelfa@student.chalmers.se>
2022-09-29 15:00:23 +0200

    Javadoc för SpawnEnemies

--- src/Model/SpawnEnemies.java
+++ src/Model/SpawnEnemies.java
@@ -2,6 +2,11 @@ package Model;
 
 import Model.Entities.AddEnemy;
 
+/**
+ * The class responsible for spawning enemies every new round.
+ *
+ * @author Samuel Falck
+ */
 class SpawnEnemies implements Runnable {
 
     private final Game game;
@@ -9,6 +14,14 @@ class SpawnEnemies implements Runnable {
     private final int round;
     private final int difficulty;
 
+    /**
+     * Creates an instance of SpawnEnemies.
+     *
+     * @param game                  reference to the instance of Game.
+     * @param enemyEntityCreator    reference to the instance of EntityCreator.
+     * @param round                 the current round.
+     * @param difficulty            the difficulty.
+     */
     SpawnEnemies(Game game, AddEnemy enemyEntityCreator, int round, int difficulty) {
         this.game = game;
         this.enemyEntityCreator = enemyEntityCreator;
@@ -16,6 +29,9 @@ class SpawnEnemies implements Runnable {
         this.difficulty = difficulty;
     }
 
+    /**
+     * Spawns enemies. Should run a couple of seconds after the previous round ended.
+     */
     @Override
     public void run() {
         int numberOfNewEnemies = numberOfNewEnemies();
@@ -26,7 +42,15 @@ class SpawnEnemies implements Runnable {
         System.out.println("ENEMIES SPAWNED: " + numberOfNewEnemies); // Debug
     }
 
+    /**
+     * Calculate and return the number if enemies to be spawned this round.
+     *
+     * @return 1 if it is the first round. Otherwise, (int) (round^2)*difficulty/2.
+     */
     private int numberOfNewEnemies() {
-        return (int) (Math.pow(round, 2) * difficulty);
+        if (round == 1) {
+            return 1;
+        }
+        return (int) (Math.pow(round, 2)*difficulty/2);
     }
 }
