14ec3f8606925ea6cdbf094560dbe948dee7720e
alfred <alfred.berglof@gmail.com>
2022-09-29 11:29:30 +0200

    pause is se trough black screen

--- src/View/PauseMenuPanel.java
+++ src/View/PauseMenuPanel.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class PauseMenuPanel extends JPanel {
     public PauseMenuPanel(Dimension size){
         setSize(size);
-        setBackground(Color.black);
+        setBackground(new Color(0,0,0,200));
         setVisible(true);
     }
 }
