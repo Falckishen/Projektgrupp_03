673e8e6b50712d9b69b2c226612d1656554a35b7
alfred <alfred.berglof@gmail.com>
2022-09-29 15:12:23 +0200

    menu split into multiple JPanels with JLabels without function

--- src/View/PauseMenuPanel.java
+++ src/View/PauseMenuPanel.java
@@ -11,20 +11,54 @@ public class PauseMenuPanel extends JPanel {
 
         setSize(size);
         setBackground(new Color(0,0,0,200));
-        setLayout(new GridLayout(3,3));
+        setLayout(new GridLayout(4,0));
 
+        JPanel topPanel = new JPanel();
+        topPanel.setBackground(Color.GREEN);
+        //topPanel.setSize(size.width, size.height/3*2);
+        JLabel text = new JLabel("hej");
+        topPanel.add(text);
+        add(topPanel,0);
+
+        JPanel middlePanel = new JPanel();
+        middlePanel.setBackground(new Color(0,0,0,0));
+        add(middlePanel,1);
+        JPanel middlePanel1 = new JPanel();
+        middlePanel1.setBackground(new Color(0,0,0,0));
+        add(middlePanel1,2);
+
+        JPanel botPanel = new JPanel();
+        botPanel.setBackground(Color.yellow);
+        //botPanel.setSize(size.width,size.height/3);
+        botPanel.setLayout(new GridLayout(0,2));
+
+        JPanel botLeft = new JPanel();
+        botLeft.setBackground(Color.GRAY);
+        Label quitLabel = new Label("Quit");
+
+        botLeft.add(quitLabel);
+        botPanel.add(botLeft);
+
+        JPanel botRight = new JPanel();
+        botRight.setBackground(Color.DARK_GRAY);
+        botPanel.add(botRight);
+
+        /*
         JButton button1 = new JButton(buttonPressed);
         //button1.setSize(200,200);
         button1.setBackground(Color.BLUE);
+        button1.setBorder(null);
+        JLabel text2 = new JLabel();
+        text2.setText("Press me");
+        button1.add(text2);
+        botPanel.add(button1);
 
-        JLabel text = new JLabel();
-        text.setText("Press me");
-        button1.add(text);
+        botPanel.add(new JButton("Button 1"));
+        botPanel.add(new JButton("Button 2"));
+        //botPanel.add(new JButton("Button 3"));
+         */
 
-        add(button1);
-        add(button1);
-        add(button1);
-
+        add(botPanel,3);
         setVisible(true);
     }
 
