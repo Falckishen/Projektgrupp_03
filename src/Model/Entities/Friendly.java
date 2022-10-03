684252769bae75a05bc05d4088f9c77185d168a5
Marsaielda <ida.altenstedt@gmail.com>
2022-10-03 10:19:50 +0200

    player hitbox fix

--- src/Model/Entities/Friendly.java
+++ src/Model/Entities/Friendly.java
@@ -5,7 +5,7 @@ import Utilities.EntityType;
 abstract class Friendly extends MovableEntity {
 
     protected Friendly(EntityType entityType, int x, int y, int hitBoxRadiusX, int hitBoxRadiusY, int speed, int health) {
-        super(entityType, x, y, hitBoxRadiusX, hitBoxRadiusY, speed, health);
+        super(entityType, hitBoxRadiusX, hitBoxRadiusY, x, y, speed, health);
     }
 
     protected void CollidedWithEnemy(int attackPower){
