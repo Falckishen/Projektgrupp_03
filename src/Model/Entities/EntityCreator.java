9aef59c84ab0d93c89e998229641617d30015369
Falckishen <samuelfa@student.chalmers.se>
2022-10-06 13:59:00 +0200

    Små ändringar

--- src/Model/Entities/EntityCreator.java
+++ src/Model/Entities/EntityCreator.java
@@ -103,15 +103,6 @@ public class EntityCreator implements AddProjectile, AddEnemy, AddFriendly, AddN
 
     /*--------------------------------------- AddEnemy (used by Game class) ----------------------------------------*/
 
-    @Override
-    public void createMonster() {
-        int health = 1*difficulty;
-        int attackPower = 1*difficulty;
-        Monster m = new Monster(getXCoordinateInWorld(), getYCoordinateInWorld(), friendlies, 3, health, attackPower);
-        enemies.add(m);
-        tickObservers.add(m);
-    }
-
     /**
      * checks for a random x coordinate that isn't too close to the player.
      * @return int x
