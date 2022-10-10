6be5e6e3682a841feb3bb78d143b69a34de15db3
alfred <alfred.berglof@gmail.com>
2022-10-10 12:12:41 +0200

    a work in progress

--- test/Controller/KeyboardInputTest.java
+++ test/Controller/KeyboardInputTest.java
@@ -4,6 +4,8 @@ import Model.MainMenu;
 import View.GameView;
 import org.junit.jupiter.api.*;
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,39 +14,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class KeyboardInputTest {
     List playerInputList;
+    Robot robot;
+    MainMenu mainMenu;
+    GameView mainView;
+    KeyboardInput input;
     @BeforeEach
-    void initClasses(){
-        MainMenu mainMenu = new MainMenu("testWorld", 1000);
-        GameView mainView = new GameView(mainMenu, 1000, 800);
-        var keyboardInput = new KeyboardInput(mainView.getFrameRootPane(), mainMenu.getPlayerInputList(), mainMenu, mainView, mainMenu.getWeaponInputList());
+    void initClasses() throws AWTException {
+        mainMenu = new MainMenu("testWorld", 1000);
+        mainView = new GameView(mainMenu, 1000, 800);
+        input = new KeyboardInput(mainView.getFrameRootPane(), mainMenu.getPlayerInputList(), mainMenu, mainView, mainMenu.getWeaponInputList());
         playerInputList = mainMenu.getPlayerInputList();
+        robot = new Robot();
         mainView.showMainMenu();
     }
     @Test
     void wInputTest() throws InterruptedException {
-        boolean loop = true;
-        System.out.println("Press and hold W");
-        while (loop){
-            for (Object o : playerInputList) {
-                if ((int)o == KeyEvent.VK_W){
-                    loop = false;
-                }
+        robot.keyPress(KeyEvent.VK_W);
+        for (Object o : playerInputList) {
+            if ((int)o == KeyEvent.VK_W){
+                assertEquals(1,1);
             }
-            Thread.sleep(100);
         }
     }
     @Test
     void aInputTest() throws InterruptedException {
-        boolean loop = true;
-        System.out.println("Press and hold A");
-        while (loop){
-            for (Object o : playerInputList) {
-                if ((int)o == KeyEvent.VK_A){
-                    loop = false;
-                }
+        boolean test = false;
+        for (Object o : mainMenu.getPlayerInputList()) {
+            System.out.println(o);
+            if ((int) o == KeyEvent.VK_A) {
+                test = true;
+                break;
             }
-            Thread.sleep(100);
         }
+        robot.keyRelease(KeyEvent.VK_A);
+        assertTrue(test);
     }
     @Test
     void sInputTest() throws InterruptedException {
