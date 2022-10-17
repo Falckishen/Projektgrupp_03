2b9d3fa146dc2f6aa356e92f78a5a822e83e3b9d
alfred <alfred.berglof@gmail.com>
2022-10-17 10:19:43 +0200

    uneccesary code

--- src/Model/Weapons/WeaponFactory.java
+++ src/Model/Weapons/WeaponFactory.java
@@ -14,9 +14,4 @@ public class WeaponFactory {
         return new SingleShooter(projectileList, playerPosition,3,15,
                 40, 1, keyInputs);
     }
-
-    public static Weapon getTestWeapon(AddProjectile projectileList, Position playerPosition, List<Integer> keyInputs){
-        return new SingleShooter(projectileList, playerPosition,2,50,
-                60, 1, keyInputs);
-    }
 }
