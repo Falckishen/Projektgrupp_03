40c41fa7268cb49292c9761ed3f5890bac20ef38
Falckishen <samuelfa@student.chalmers.se>
2022-10-24 20:18:29 +0200

    Javadoc ButtonHandler

--- src/main/java/Controller/ButtonHandler.java
+++ src/main/java/Controller/ButtonHandler.java
@@ -9,22 +9,41 @@ public class ButtonHandler {
 
     private final MainMenu mainMenu;
 
+    /**
+     * Creates an instance of ButtonHandler.
+     *
+     * @param mainMenu reference to the MainMenu.
+     */
     public ButtonHandler(MainMenu mainMenu) {
         this.mainMenu = mainMenu;
     }
 
+    /**
+     * Starts a game.
+     */
     public void startGame() {
         mainMenu.startGame();
     }
 
+    /**
+     * The main menu appears.
+     */
     public void showMainMenu() {
         mainMenu.showMainMenu();
     }
 
+    /**
+     * The application shuts down.
+     */
     public void quitApplication() {
         mainMenu.quitApplication();
     }
 
+    /**
+     * Sets a new difficulty.
+     *
+     * @param difficulty the new difficulty.
+     */
     public void setDifficulty(int difficulty) {
         mainMenu.setDifficulty(difficulty);
     }
